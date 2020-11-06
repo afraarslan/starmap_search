b76e842679248c08ccec796f2df05bbc2d3e25e3
Afra Akbaş <akbasafra.aa@gmail.com>
2020-11-06 15:56:53 +0300

    extract features

--- solution.py
+++ solution.py
@@ -1,9 +1,29 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import cv2 as cv2
-#import matplotlib.pyplot as plt
+import numpy as np              # linear algebra
+import cv2 as cv                #opencv
+import matplotlib.pyplot as plt #plot images
 import os
 
 
-img1 = cv2.imread('Small_area.png',0)          # queryImage
-img2 = cv2.imread('StarMap.png',0) # trainImage
+img1 = cv.imread('Small_area.png', cv.IMREAD_GRAYSCALE)       # queryImage
+img2 = cv.imread('StarMap.png', cv.IMREAD_GRAYSCALE)          # trainImage
+
+MIN_MATCH_COUNT = 10
+
+# Initiate SIFT detector
+sift = cv.SIFT_create()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
+print('detected and computed keypoints')
+
+bf = cv.BFMatcher()
+matches = bf.knnMatch(des1, des2, k=2)
+
+# store all the good matches as per Lowe's ratio test.
+good = []
+for m,n in matches:
+    if m.distance < 0.75*n.distance:
+        good.append(m)
+        
+print('stored good matches')
