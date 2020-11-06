b8c0f91cd11e3f00109cbb62ba033f446027671f
Afra Akbaş <akbasafra.aa@gmail.com>
2020-11-06 16:52:59 +0300

    find homograhy

--- solution.py
+++ solution.py
@@ -20,10 +20,39 @@ print('detected and computed keypoints')
 bf = cv.BFMatcher()
 matches = bf.knnMatch(des1, des2, k=2)
 
-# store all the good matches as per Lowe's ratio test.
+# store all the good matches as per ratio test.
 good = []
 for m,n in matches:
     if m.distance < 0.75*n.distance:
         good.append(m)
         
 print('stored good matches')
+
+LENGTH_GOOD_MATCH = len(good)
+
+if LENGTH_GOOD_MATCH > MIN_MATCH_COUNT:
+    print("found enough matched - %d/%d" % (LENGTH_GOOD_MATCH, MIN_MATCH_COUNT))
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+    M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
+    matchesMask = mask.ravel().tolist()
+
+    h,w = img1.shape
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+    dst = cv.perspectiveTransform(pts,M)
+
+    img2 = cv.polylines(img2,[np.int32(dst)],True,255,3, cv.LINE_AA)
+
+else:
+    print ("Not enough matches are found - %d/%d" % (LENGTH_GOOD_MATCH, MIN_MATCH_COUNT))
+    matchesMask = None
+    
+draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers
+                   flags = 2)
+
+img3 = cv.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+
+plt.imshow(img3, 'gray'),plt.show()
