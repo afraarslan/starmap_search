7658674321322fa1a1f4d93b1e1c7716e981670a
Afra Akbaş <akbasafra.aa@gmail.com>
2020-11-06 17:49:26 +0300

    parse arguments

--- solution.py
+++ solution.py
@@ -2,57 +2,86 @@ import numpy as np              # linear algebra
 import cv2 as cv                #opencv
 import matplotlib.pyplot as plt #plot images
 import os
+import math
+import argparse
 
 
-img1 = cv.imread('Small_area.png', cv.IMREAD_GRAYSCALE)       # queryImage
-img2 = cv.imread('StarMap.png', cv.IMREAD_GRAYSCALE)          # trainImage
+def find_features(small_image_path, starmap_path):
+    small_img = cv.imread(small_image_path, cv.IMREAD_GRAYSCALE)       # queryImage
+    starmap_img = cv.imread(starmap_path, cv.IMREAD_GRAYSCALE)          # trainImage
 
-MIN_MATCH_COUNT = 10
+    MIN_MATCH_COUNT = 10
 
-# Initiate SIFT detector
-sift = cv.SIFT_create()
+    # Initiate SIFT detector
+    sift = cv.SIFT_create()
 
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
-print('detected and computed keypoints')
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(small_img,None)
+    kp2, des2 = sift.detectAndCompute(starmap_img,None)
 
-bf = cv.BFMatcher()
-matches = bf.knnMatch(des1, des2, k=2)
+    # Initiate BF matcher
+    bf = cv.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
 
-# store all the good matches as per ratio test.
-good = []
-for m,n in matches:
-    if m.distance < 0.75*n.distance:
-        good.append(m)
+    # store all the good matches as per ratio test.
+    good = []
+    for m,n in matches:
+        if m.distance < 0.75*n.distance:
+            good.append(m)
+            
+    length_good_match = len(good)
+
+    if length_good_match > MIN_MATCH_COUNT:
+        print("found enough matched - %d/%d" % (length_good_match, MIN_MATCH_COUNT))
+        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+        M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
+        matchesMask = mask.ravel().tolist()
+
+        h,w = small_img.shape
+        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+        dst = cv.perspectiveTransform(pts,M)
+
+        angle = - math.atan2(M[1,0], M[0,0]) * 180 / math.pi
+        print("Rotation angle ", angle)
+        print("Corner points of small image onto StarMap ")
+        print(np.int32(dst))
+
+        starmap_img = cv.polylines(starmap_img,[np.int32(dst)],True,255,3, cv.LINE_AA)
+
+    else:
+        print ("Not enough matches are found - %d/%d" % (length_good_match, MIN_MATCH_COUNT))
+        matchesMask = None
         
-print('stored good matches')
+    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                    singlePointColor = None,
+                    matchesMask = matchesMask, # draw only inliers
+                    flags = 2)
 
-LENGTH_GOOD_MATCH = len(good)
+    img3 = cv.drawMatches(small_img,kp1,starmap_img,kp2,good,None,**draw_params)
 
-if LENGTH_GOOD_MATCH > MIN_MATCH_COUNT:
-    print("found enough matched - %d/%d" % (LENGTH_GOOD_MATCH, MIN_MATCH_COUNT))
-    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+    plt.imshow(img3, 'gray'),plt.show()
 
-    M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
-    matchesMask = mask.ravel().tolist()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--small-image-path",
+        type=str,
+        help="one of the small images"
+    )
 
-    h,w = img1.shape
-    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-    dst = cv.perspectiveTransform(pts,M)
+    parser.add_argument(
+        "--starmap-path",
+        type=str,
+        help="the star map image"
+    )
 
-    img2 = cv.polylines(img2,[np.int32(dst)],True,255,3, cv.LINE_AA)
+    args = parser.parse_args()
+    small_image_path = args.small_image_path
+    starmap_path = args.starmap_path
 
-else:
-    print ("Not enough matches are found - %d/%d" % (LENGTH_GOOD_MATCH, MIN_MATCH_COUNT))
-    matchesMask = None
-    
-draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                   singlePointColor = None,
-                   matchesMask = matchesMask, # draw only inliers
-                   flags = 2)
-
-img3 = cv.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
-
-plt.imshow(img3, 'gray'),plt.show()
+    if not small_image_path or not starmap_path:
+        print("a small image and the starmap are required")
+    else:
+        find_features(small_image_path, starmap_path)
